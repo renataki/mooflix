91e9c82b45c7c3b5821c69e4a21b63b1f2f853a3
renataki <renataki@preloode.com>
2021-06-26 13:44:13 +0700

    Add primary route
    Add primary route

--- panel/views.py
+++ panel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(response):
+    return render(response, "panel/index.html")
+
+#def index(response, id):
+    #return HttpResponse("<h1>Hello World</h1>")
--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(response):
+    return render(response, "website/index.html")
+
+#def index(response, id):
+    #return HttpResponse("<h1>Hello World</h1>")
